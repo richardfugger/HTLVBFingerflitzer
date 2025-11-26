5f918a0b9fd13aedac753ebbd33dfdab615e42db
richardfugger <richard.fugger@gmail.com>
2025-11-26 14:52:33 +0100

    commit

--- create-webapp.ps1
+++ create-webapp.ps1
@@ -1,44 +1,76 @@
+az login
+gh auth login
+
 $UserName = ((az ad signed-in-user show | ConvertFrom-Json).userPrincipalName -replace '@.*$','' -replace '\W','').ToLower()
+az group create --name rg-fingerflitzer --location swedencentral |Out-Null
 
 az appservice plan create `
-  --name asp-beer4me `
-  --sku B1 `
+  --name asp-fingerflitzer `
+  --sku P0V3 `
   --is-linux `
-  --resource-group rg-beer4me | Out-Null
+  --resource-group rg-fingerflitzer | Out-Null
 
 az webapp create `
-  --name wa-beer4me-$UserName `
+  --name wa-fingerflitzer-$UserName `
   --runtime DOTNETCORE:8.0 `
   --assign-identity `
   --https-only true `
   --public-network-access Enabled `
-  --plan asp-beer4me `
-  --resource-group rg-beer4me | Out-Null
+  --plan asp-fingerflitzer `
+  --resource-group rg-fingerflitzer | Out-Null
+
+  $SubscriptionId = (az account show | ConvertFrom-Json).id
+  $ServicePricipalSecret = az ad sp create-for-rbac `
+    --name "gh-action-to-deploy-fingerflitzer-webapp-$UserName" `
+    --role contributor `
+    --scopes /subscriptions/$SubscriptionId/resourceGroups/rg-fingerflitzer/providers/Microsoft.Web/sites/wa-fingerflitzer-$UserName `
+    --json-auth
+
+
+
+gh secret set AZURE_CREDENTIALS `
+  --repo richardfugger/HTLVBFingerflitzer `
+  --body "$ServicePricipalSecret"
+
+az webapp deployment slot create `
+  --slot staging `
+  --name wa-fingerflitzer-$UserName `
+  --resource-group rg-fingerflitzer
+
+
 
 # Allow access from web app to database
 # see https://learn.microsoft.com/en-us/azure/app-service/tutorial-connect-msi-azure-database
-az extension add --name serviceconnector-passwordless --upgrade
-az webapp connection create postgres-flexible `
-  --connection beer4me_webapp `
-  --resource-group rg-beer4me `
-  --name wa-beer4me-$UserName `
-  --target-resource-group rg-beer4me `
-  --server db-beer4me-$UserName `
-  --database beer4me `
-  --system-identity `
-  --client-type dotnet | Out-Null
+# az extension add --name serviceconnector-passwordless --upgrade
+# az webapp connection create postgres-flexible `
+#   --connection fingerflitzer_webapp `
+#   --resource-group rg-fingerflitzer `
+#   --name wa-fingerflitzer-$UserName `
+#   --target-resource-group rg-fingerflitzer `
+#   --server db-fingerflitzer-$UserName `
+#   --database fingerflitzer `
+#   --system-identity `
+#   --client-type dotnet | Out-Null
 
-az extension add --name rdbms-connect
-$User = az ad signed-in-user show | ConvertFrom-Json
-$AccessToken = az account get-access-token --resource-type oss-rdbms | ConvertFrom-Json
-az postgres flexible-server execute `
-  --querytext "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO `"aad_beer4me_webapp`";GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO `"aad_beer4me_webapp`";" `
-  --database-name beer4me `
-  --admin-user $User.userPrincipalName `
-  --admin-password $AccessToken.accessToken `
-  --name db-beer4me-$UserName
+# az extension add --name rdbms-connect
+# $User = az ad signed-in-user show | ConvertFrom-Json
+# $AccessToken = az account get-access-token --resource-type oss-rdbms | ConvertFrom-Json
+# az postgres flexible-server execute `
+#   --querytext "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO `"aad_fingerflitzer_webapp`";GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO `"aad_fingerflitzer_webapp`";" `
+#   --database-name fingerflitzer `
+#   --admin-user $User.userPrincipalName `
+#   --admin-password $AccessToken.accessToken `
+#   --name db-fingerflitzer-$UserName
 
 $WebApp = az webapp show `
-  --name wa-beer4me-$UserName `
-  --resource-group rg-beer4me | ConvertFrom-Json
-Write-Host "### Web app: $($WebApp.defaultHostName)"
+  --name wa-fingerflitzer-$UserName `
+  --resource-group rg-fingerflitzer | ConvertFrom-Json
+Write-Host "### Web app: https://$($WebApp.defaultHostName)"
+
+
+<#
+az group delete --name rg-fingerflitzer --no-wait
+$ServicePrinciple = az ad sp list --display-name "gh-action-to-deploy-fingerflitzer-webapp-$UserName" | ConvertFrom-Json
+az ad sp delete --id $ServicePrinciple.id
+#>
+
